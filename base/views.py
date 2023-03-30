f2cda6b8927adf202019110a6833aa60914c1eff
sphinx-austin <austinjuniour4@gmail.com>
2023-03-30 23:29:49 +0300

    update view routes

--- base/views.py
+++ base/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/index.html')
+
+
+def about(request):
+    return render(request, 'base/about.html')
+
+
+def services(request):
+    return render(request, 'base/svs.html')
