4b616f6a57ed32dc6b5715de0186eaab5f206f27
sphinx-austin <austinjuniour4@gmail.com>
2023-04-04 13:50:49 +0300

    url routes

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('/welcome/', views.welcome, name='welcome'),
+    path('welcome/', views.welcome, name='welcome'),
+    path('survey/', views.survey, name='survey'),
 ]
