f6820ef6d20623d92af091e9bb27e8bfc44b9802
sphinx-austin <austinjuniour4@gmail.com>
2023-04-06 11:42:34 +0300

    improvements (appointment)

---
+++ appointment/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from . views import HomeAppointment, AppointmentTemplateView, ManageAppointmentTemplateView
+
+urlpatterns = [
+    path('appointment', HomeAppointment.as_view(), name='appointment'),
+
+    path('make-an-appointment/',
+         AppointmentTemplateView.as_view(), name='make_appointment'),
+
+    path('manage-appointments/',
+         ManageAppointmentTemplateView.as_view(), name='manage'),
+]
