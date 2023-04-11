7a9f3c94695ed34ca8a9009812924f128d9d0dc2
sphinx-austin <austinjuniour4@gmail.com>
2023-04-11 21:00:21 +0300

    new routes

--- base/urls.py
+++ base/urls.py
@@ -3,13 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< HEAD
+    path('vct/', views.vctPage, name='vct'),
     path('privacy/', views.privacy_policy, name='privacy_policy'),
     path('terms/', views.terms_of_use, name='terms_of_use'),
-=======
-    path('about/', views.about, name='about'),
-    path('vct/', views.vctPage, name='vct'),
->>>>>>> efcfd5a2036aba545b87caea4764f059b787ff2f
     path('welcome/', views.welcome, name='welcome'),
     path('survey/', views.survey, name='survey'),
     path('survey/predictor/', views.predictor, name='predictor'),
