2086e7050248ee199a7564b3a4df763a55da8b1c
sphinx-austin <austinjuniour4@gmail.com>
2023-05-24 13:38:10 +0300

    update urls & footer

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('vct/', views.vctPage, name='vct'),
-    path('privacy/', views.privacy_policy, name='privacy_policy'),
-    path('terms/', views.terms_of_use, name='terms_of_use'),
+    path('privacy/', views.privacy_policy, name='privacy'),
+    path('terms/', views.terms_of_use, name='terms'),
     path('welcome/', views.welcome, name='welcome'),
     path('survey/', views.survey, name='survey'),
     path('survey/predictor/', views.predictor, name='predictor'),
