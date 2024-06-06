1c136443d1a78946942debd672a0688d50fccef6
sphinx-austin <austinjuniour4@gmail.com>
2024-06-06 21:13:07 +0300

    updated

--- jijue/settings.py
+++ jijue/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'jijue.wsgi.application'
 
 if not DEBUG:
     DATABASES = {
-	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+	"default": dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
     
 else:
